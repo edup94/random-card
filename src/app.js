97449c1bb176a38115b376a4deefb9370a21f928
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-04-22 15:00:21 +0000

    guardado1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let figure = ["♥", "♦", "♣", "♠"];
+  let character = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let figureIndex = Math.floor(Math.random() * figure.length);
+  let characterIndex = Math.floor(Math.random() * character.length);
+
+  document.querySelector("#figura1").innerHTML = `<h1>
+            ${figure[figureIndex]}</h1>`;
+  document.querySelector("#numero").innerHTML = `<h1>
+            ${character[characterIndex]}</h1>`;
+  document.querySelector("#figura2").innerHTML = `<h1>
+            ${figure[figureIndex]}</h1>`;
+
+  if (figure == "♦" || figure == "♥") {
+    document.getElementById("figure1", "figure2").style.color = "red";
+    document.getElementById("numero").style.color = "red";
+  }
 };
